44309fd6d6d0d9aa968d37a351fb72cf1732058a
arunramana <arunr@bu.edu>
2026-02-16 20:39:15 -0500

    mvp init

--- minimal/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Minimal DELLM (Distributed Evolutionary LLM) implementation."""
-from minimal.embedding_orchestrator import EmbeddingOrchestrator
-from minimal.decomposer import QueryDecomposer
-from minimal.router import ClusterRouter
-from minimal.embedding_assembly import EmbeddingAssembly
-from minimal.training import RLTrainer
-
-__all__ = ["EmbeddingOrchestrator", "QueryDecomposer", "ClusterRouter", "EmbeddingAssembly", "RLTrainer"]
-
